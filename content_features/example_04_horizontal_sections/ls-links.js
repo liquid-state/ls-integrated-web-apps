f44dd543e9a89365e120424251da085128812159
Cyril Doussin <cyril@liquid-state.com>
2017-04-28 15:51:39 +1000

    Add fourth example for content features: horizontal sections with background images

---
+++ content_features/example_04_horizontal_sections/ls-links.js
@@ -0,0 +1,35 @@
+// intercept any anchor with a class of "ls-link" and turn it into an LS action
+// for the mobile app to open in an in-app browser
+$(function() {
+    $('.ls-link').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var $target = $(e.target);
+        if (!$target.hasClass('ls-link')) {
+            $target = $target.closest('.ls-link'); 
+        }
+        var url = $target.attr('href');
+        if (!url) {
+            url = $target.attr('data-href');
+        }
+        if (!url) {
+            return;
+        }
+        var item_definition = {item_type: 'web', url: url, settings: {}};
+        if ($target.hasClass('ls-link-external')) {
+            item_definition['launch_type'] = 'external';
+        }
+        if ($target.hasClass('ls-link-push')) {
+            // needed to open in-app browser as a pushed view and not in a modal
+            item_definition['settings'] = {'ios_transition': 'push'};
+        }
+        if ($target.hasClass('ls-link-attachments')) {
+            // needed for sites which users should be able to upload files to
+            // on iOS: ls-link-push should also be specified for the upload UI controls to work
+            item_definition['settings'] = {'enableFileAccess': "yes"};
+        }
+        json_definition = JSON.stringify(item_definition);
+        var new_href = 'liquidstate://action/?definition=' + encodeURIComponent(json_definition);
+        document.location.href = new_href;
+    });
+});
